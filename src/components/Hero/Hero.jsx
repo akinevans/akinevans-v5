9e43b9ad7203789ba056e69d9de67cc78e65dbfd
Akin Evans <akinevans@gmail.com>
2024-10-03 13:26:37 -0700

    Build Nav component, begin Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className='hero-wrapper'>
+      <h1 className='heading'>Akin Evans</h1>
+      <h2 className='subheading'>
+        <span className='highlight'>Front-End Developer</span> passionate about
+        building beautiful user interfaces
+      </h2>
+    </div>
+  );
+}
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav className='nav'>
+      <a href='' className='nav-link'>
+        Home
+      </a>
+      <a href='' className='nav-link'>
+        Projects
+      </a>
+      <a href='' className='nav-link'>
+        Resume
+      </a>
+      <a href='' className='nav-link'>
+        Contact
+      </a>
+    </nav>
+  );
+}
