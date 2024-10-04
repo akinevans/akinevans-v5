a6f4af68ccd99136abc6d245a8209190e87bf949
Akin Evans <akinevans@gmail.com>
2024-10-04 15:02:17 -0700

    Ad bottom margin to ProjectItem

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,34 @@ function App() {
           githubLink='https://github.com/akinevans/cafe-nyleta'
           externalLink='https://cafe-nyleta.netlify.app/#/shop'
         />
+        <ProjectItem
+          projectImg={cafe}
+          alt='cafe nyleta'
+          title='Cafe Nyleta'
+          description='A Full-Stack application for minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat nostrud iu tep. Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat nostrud iu tep.'
+          listItem1='React'
+          listItem2='JavaScript'
+          listItem3='SCSS'
+          listItem4='Example'
+          listItem5='Example'
+          listItem6='Example'
+          githubLink='https://github.com/akinevans/cafe-nyleta'
+          externalLink='https://cafe-nyleta.netlify.app/#/shop'
+        />
+        <ProjectItem
+          projectImg={cafe}
+          alt='cafe nyleta'
+          title='Cafe Nyleta'
+          description='A Full-Stack application for minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat nostrud iu tep. Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat nostrud iu tep.'
+          listItem1='React'
+          listItem2='JavaScript'
+          listItem3='SCSS'
+          listItem4='Example'
+          listItem5='Example'
+          listItem6='Example'
+          githubLink='https://github.com/akinevans/cafe-nyleta'
+          externalLink='https://cafe-nyleta.netlify.app/#/shop'
+        />
       </div>
       <SectionTitle title='Experience' />
       <SectionTitle title='Education' />
