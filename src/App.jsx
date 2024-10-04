f30e4bbca67cef76b338bb9a5284b889180ac3fb
Akin Evans <akinevans@gmail.com>
2024-10-04 12:24:25 -0700

    Build Section Title Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Hero from "./components/Hero/Hero";
+import SectionTitle from "./components/SectionTitle/SectionTitle";
 
 function App() {
   return (
     <div className='app-wrapper'>
       <Nav />
       <Hero />
+      <SectionTitle title='Projects' />
+      <SectionTitle title='Experience' />
+      <SectionTitle title='Education' />
+      <SectionTitle title='Contact' />
     </div>
   );
 }
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,5 @@
+import "./SectionTitle.css";
+
+export default function SectionTitle(props) {
+  return <h1 className='section-title'>{props.title}</h1>;
+}
