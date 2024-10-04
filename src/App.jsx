a1eb9f3bef7542e07276b781bab04eb7f7a89ae5
Akin Evans <akinevans@gmail.com>
2024-10-04 14:33:16 -0700

    Build ProjectItem component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Hero from "./components/Hero/Hero";
 import SectionTitle from "./components/SectionTitle/SectionTitle";
+import ProjectItem from "./components/Project/ProjectItem";
+import cafe from "./assets/cafe.svg";
 
 function App() {
   return (
@@ -9,6 +11,20 @@ function App() {
       <Nav />
       <Hero />
       <SectionTitle title='Projects' />
+      <ProjectItem
+        projectImg={cafe}
+        alt='cafe nyleta'
+        title='Cafe Nyleta'
+        description='A Full-Stack application for minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat nostrud iu tep. Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat nostrud iu tep.'
+        listItem1='React'
+        listItem2='JavaScript'
+        listItem3='SCSS'
+        listItem4='Example'
+        listItem5='Example'
+        listItem6='Example'
+        githubLink='https://github.com/akinevans/cafe-nyleta'
+        externalLink='https://cafe-nyleta.netlify.app/#/shop'
+      />
       <SectionTitle title='Experience' />
       <SectionTitle title='Education' />
       <SectionTitle title='Contact' />
---
+++ src/components/Project/ProjectItem.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import github from "../../assets/github.svg";
+import external_link from "../../assets/external-link.svg";
+import "./ProjectItem.css";
+
+export default function ProjectItem(props) {
+  return (
+    <div className='project-wrapper'>
+      <div className='left'>
+        <img src={props.projectImg} alt={props.alt} className='project-img' />
+      </div>
+      <div className='right'>
+        <h1 className='project-title'>{props.title}</h1>
+        <p className='project-description'>{props.description}</p>
+        <ul className='project-list'>
+          <div className='list-left'>
+            <li className='project-list-item'>{props.listItem1}</li>
+            <li className='project-list-item'>{props.listItem2}</li>
+            <li className='project-list-item'>{props.listItem3}</li>
+          </div>
+          <div className='list-right'>
+            <li className='project-list-item'>{props.listItem4}</li>
+            <li className='project-list-item'>{props.listItem5}</li>
+            <li className='project-list-item'>{props.listItem6}</li>
+          </div>
+        </ul>
+        <div className='project-links-wrapper'>
+          <Link className='github-img' target='_blank' to={props.githubLink} />
+          <Link
+            className='external-link-img'
+            target='_blank'
+            to={props.externalLink}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
