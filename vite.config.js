804369036d533b9fc101383f39aa05ab5b537b12
Akin Evans <akinevans@gmail.com>
2024-10-14 12:14:09 -0700

    update vit config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/akinevans.com-v5",
   plugins: [react()],
+  base: "/akinevans.com-v5",
 });
