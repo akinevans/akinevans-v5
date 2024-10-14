f53525b1813fc985a02571734c0edc0cdbde591e
Akin Evans <akinevans@gmail.com>
2024-10-14 13:31:47 -0700

    update vite config and package json

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/akinevans.com-v5/",
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true, // also necessary
+  },
 });
