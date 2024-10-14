38a18fd08ddcb4f1842bdc7a46f35decbe4156ce
Akin Evans <akinevans@gmail.com>
2024-10-14 13:39:21 -0700

    remove github/workflows, update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/akinevans.com-v5/",
-  build: {
-    outDir: "./dist",
-    emptyOutDir: true, // also necessary
-  },
+  // build: {
+  //   outDir: "./dist",
+  //   emptyOutDir: true,
+  // },
 });
