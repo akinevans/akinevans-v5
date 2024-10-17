8edeca768725a8dd340cd9c178294f96d33096b8
Akin Evans <akinevans@gmail.com>
2024-10-17 15:52:15 -0700

    update homepage base name

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <RouterProvider router={router} /> */}
 
-    <BrowserRouter basename='/akinevans-v5/'>
+    <BrowserRouter basename='/'>
       <Routes>
         <Route path='/' element={<App />} />
       </Routes>
