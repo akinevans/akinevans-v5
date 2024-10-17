e259e56423535fca81761d70d0a2e9746cfee5ff
Akin Evans <akinevans@gmail.com>
2024-10-17 15:53:48 -0700

    revert last changes

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <RouterProvider router={router} /> */}
 
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/akinevans-v5/'>
       <Routes>
         <Route path='/' element={<App />} />
       </Routes>
