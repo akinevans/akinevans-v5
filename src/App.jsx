fbe855e5ad07658157cedb50e1390b43df7ba883
Akin Evans <akinevans@gmail.com>
2024-10-24 09:33:52 -0700

    Update various styling to accomodate smaller screens. Fix right margin for projects component. Fix flex gap between DataCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import Homepage from "./pages/Homepage";
 import "./App.css";
 
-//FIXME: height and  on project images broken on mobile (pet pal), try converting to SVG then removing its media queries
-
-//FIXME: width broken on mobile
-
 function App() {
   return <Homepage />;
 }
